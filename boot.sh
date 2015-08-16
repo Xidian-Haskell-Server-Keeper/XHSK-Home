74818bc16bd782be802e08b3cb7405384e80aca6
Qinka <qinka@live.com>
2015-08-16 11:16:17 +0800

    for test again

--- boot.sh
+++ boot.sh
@@ -9,6 +9,29 @@
 function cabal_install ()
 {
   echo "XHSK-Home 编译"
-  cabal install XHSK-Home/ -O2 --make | grep 'XHSK-Home' Install*
-  echo "编译完成"
+  CIOP=$(cabal install XHSK-Home/ | grep "XHSK-Home" Install* |cat)
+  if [-n "$CIOP"]
+    then
+      echo "编译完成"
+      return 1
+    else
+      echo "编译失败"
+      return 0
+}
+
+function git_pull ()
+{
+  echo "Git Pull"
+
+}
+
+function isUPtpDATE ()
+{
+  GPOUTPUT=$(git pull origin | grep "Already up-to-date.")
+  if [-n "$GPOUTPUT"]
+    then
+    echo $GPOUTPUT
+    return 1
+  else
+    echo 
 }
