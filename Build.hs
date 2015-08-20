0337838af9db3a78f30ecbf93f7a0ffd58dcb9be
Qinka <qinka@live.com>
2015-08-20 08:53:19 +0200

    把 unix 与 windows 上部署的脚本统一了

--- Build.hs
+++ Build.hs
@@ -1,13 +1,11 @@
 #!/bin/runghc
 
-{-# LANGUAGE CPP #-}
-#define _BUILD_
 module Build where
 import Distribution.Simple
 import System.Environment
 import System.Process
-
-
+import System.Directory
+import System.Exit
 
 
 cabal = defaultMainArgs
@@ -43,11 +41,46 @@ windowsMain arg = do
         proc "./.cabal-sandbox/bin/XHSK-Home.Bin.exe" []
       createProcess $
         shell "start http://localhost:3000/"
+      putStrLn "键入回车退出"
       getLine
       (_,_,_,kill) <- createProcess $ shell "taskkill /IM XHSK-Home.Bin.exe /F"
       waitForProcess kill
       putStrLn "测试结束"
-
     _ -> undefined
 unixMain :: String -> IO ()
-unixMain _ =undefined
+unixMain arg = do
+  let flag = read arg :: Run
+  case flag of
+    Start -> start'git
+    Stop -> stopIt
+    Restart -> restartIt
+    _ -> undefined
+  where
+    start'git = do
+      (_,_,_,git'pull) <- createProcess $
+        proc "git" ["pull","master"]
+      exCode <- waitForProcess git'pull
+      case exCode of
+        ExitSuccess -> start'cabal
+        _ -> putStrLn "repo 同步失败"
+      return ()
+    start'cabal =do
+      setCurrentDirectory ".."
+      (_,_,_,cabal'install) <- createProcess $
+        proc "cabal" ["install","./XHSK-Home/"]
+      exCode <- waitForProcess cabal'install
+      case exCode of
+        ExitSuccess -> start'server
+        _ -> putStrLn "cabal 安装失败"
+      return ()
+    start'server = do
+      createProcess $
+        shell "exec ../.cabal-sandbox/bin/XHSK-Home.Bin"
+      return ()
+    stopIt = do
+      createProcess $ shell "pidof XHSK-Home.Bin | xargs kill -s 9"
+      return ()
+    restartIt = do
+      stopIt
+      start'git
+      return ()
