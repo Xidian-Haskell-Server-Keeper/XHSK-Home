5a9894507ade61f053cafc19a71a650ab06df6dc
Qinka <qinka@live.com>
2015-09-08 21:32:10 +0800

    add a part of these which is using blaze-shields, but SvgShields is missing the instance of Read.

--- src/SvgImg.hs
+++ src/SvgImg.hs
@@ -1,20 +1,32 @@
-{-#LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GADTs #-}
 
 
 
 
 module SvgImg
     (
-    --frameScotty,
     getSvgPath,
-    getSvgLinked
+    getSvgLinked,
+    SvgShields(..),
+    eval
     ) where
 
 
-      import Text.Blaze(AttributeValue,stringValue)
+      import Text.Blaze(AttributeValue,stringValue,ToMarkup)
       import Text.Blaze.Html(Html,(!))
       import Text.Blaze.Html5(a,img)
       import Text.Blaze.Html5.Attributes(href,src)
+      import Text.Blaze.Svg(Svg)
+      import Text.Blaze.Svg.Shields(
+        plasticStyle,
+        flatStyle,
+        socialStyle,
+        flatSquareStyle
+        )
+      import Data.Text.Lazy(unpack)
+      import Data.Text.Internal.Lazy(Text)
+
 
       getSvgPath :: String -> AttributeValue
       getSvgPath = stringValue . ("/site-status/d04f22c10b926df403ded5aca2668ad4/"++)
@@ -24,43 +36,31 @@ module SvgImg
         a ! href (stringValue x) $
           img ! src (getSvgPath y)
 
+      data SvgShields a b where
+        PlasticStyle ::(Show a,ToMarkup a,Read a,Show b,Floating b,Read b) =>
+                       (a,b) -> (a,b) -> Maybe String -> Maybe String -> SvgShields a b
+        FlatStyle ::(Show a,ToMarkup a,Read a,Show b,Floating b,Read b) =>
+                    (a,b) -> (a,b) -> Maybe String -> Maybe String -> SvgShields a b
+        FlatSquareStyle ::(Show a,ToMarkup a,Read a,Show b,Floating b,Read b) =>
+                          (a,b) -> (a,b) -> Maybe String -> Maybe String -> SvgShields a b
+        SocialStyle ::(Show a,ToMarkup a,Read a,Show b,Floating b,Read b) =>
+                      (a,b) -> (a,b) -> Maybe String -> Maybe String -> Maybe String -> SvgShields a b
+
+      instance (Read a,Read b) => Read (SvgShields a b) where
 
 
--- d04f22c10b926df403ded5aca2668ad4 -- svgimg's md5ßß
-{-
-      import Prelude hiding (id)
-      import Text.Blaze.Html((!))
-      import Text.Blaze.Html5.Attributes(xmlns)
-      import Text.Blaze.Svg(Svg)
-      import Text.Blaze.Svg11(
-        svg,lineargradient,stop,rect,g,path,text_
-        )
-      import Text.Blaze.Svg11.Attributes(
-        width,height,x2,y2,offset,stopColor,stopOpacity,id_,
-        rx,fill,d,textAnchor,fontFamily,fontSize,x,y,fillOpacity
-        )
-      import qualified Text.Blaze.Svg11 as S11
-      import qualified Text.Blaze.Svg11.Attributes as S11A
+      eval :: Text -> Svg
+      eval url= case ssData of
+        PlasticStyle (l,lp) (r,rp) cA cB ->
+          plasticStyle (l,lp) (r,rp) cA cB
+        FlatStyle (l,lp) (r,rp) cA cB ->
+          flatStyle (l,lp) (r,rp) cA cB
+        FlatSquareStyle (l,lp) (r,rp) cA cB ->
+          flatSquareStyle (l,lp) (r,rp) cA cB
+        SocialStyle (l,lp) (r,rp) cA cB u ->
+          socialStyle (l,lp) (r,rp) cA cB u
+        where
+          ssData = (read url') ::SvgShields Double Double
+          url' = unpack url
 
-      frameScotty :: Svg
-      frameScotty = do
-        svg ! xmlns "http://www.w3.org/2000/svg" ! width "92" ! height "20" $ do
-          lineargradient ! id_ "b" ! x2 "0" ! y2 "100%" $ do
-            stop ! offset "0" ! stopColor"#bbb" ! stopOpacity ".1"
-            stop ! offset "1" ! stopOpacity ".1"
-          S11.mask ! id_ "a" $ do
-            rect ! width "92" ! height "20" ! rx "3" ! fill "#fff"
-          g ! S11A.mask "url(#a)" $ do
-            path ! fill "#555" ! d "M0 0h46v20H0z"
-            path ! fill "#ff69b4" ! d "M46 0h46v20H46z"
-            path ! fill "url(#b)" ! d "M0 0h92v20H0z"
-          g ! fill "#fff" ! textAnchor "middle" ! fontFamily "DejaVu Sans,Verdana,Geneva,sans-serif" ! fontSize "11" $ do
-            text_ ! x "23" ! y "15" ! fill "#010101" ! fillOpacity ".3" $ do
-              "Frame"
-            text_ ! x "23" ! y "14" $ do
-              "Frame"
-            text_ ! x "68" ! y "15" ! fill "#010101" ! fillOpacity ".3" $ do
-              "Scotty"
-            text_ ! x "68" ! y "14" $ do
-              "Scotty"
--}
+-- d04f22c10b926df403ded5aca2668ad4 -- svgimg's md5
